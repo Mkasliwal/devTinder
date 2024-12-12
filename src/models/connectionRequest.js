3e42108c9920ea756946c495bbb1475df818832b
mayurkas_amdocs <mayurkas@amdocs.com>
2024-12-12 05:42:27 +0000

    feat: create connection request db model

---
+++ src/models/connectionRequest.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const connectionRequestSchema = new mongoose.Schema(
+    {
+        fromUserId: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+        },
+        toUserId: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+        },
+        status: {
+            type: String,
+            enum: {
+                values: ['interested', 'ignored', 'accepted', 'rejected'],
+                message: '{VALUE} is invalid status type',
+            },
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+connectionRequestSchema.index({
+    fromUserId: 1,
+    toUserId: 1,
+});
+
+const ConnectionRequest = mongoose.model(
+    'ConnectionRequest',
+    connectionRequestSchema
+);
+
+module.exports = ConnectionRequest;
