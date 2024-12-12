8b3b1d539db17b9e95b0882c03942e805457d0bf
mayurkas_amdocs <mayurkas@amdocs.com>
2024-12-12 05:38:25 +0000

    feat: modify profile view api

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,7 +2,19 @@ const express = require('express');
 const profileRouter = express.Router();
 const { userAuth } = require('../middlewares/auth');
 
-profileRouter.get('/profile', userAuth, async (req, res) => {   
+profileRouter.get('/profile/view', userAuth, async (req, res) => {
+    try {
+        const user = req.user;
+        res.send({
+            message: `✨ Hello, Welcome ${user.firstName}`,
+            data: user,
+        });
+    } catch (err) {
+        res.status(400).send(`⚠️ Error: ${err.message}`);
+    }
+});
+
+profileRouter.post('/profile/edit', userAuth, async (req, res) => {
     try {
         const user = req.user;
         res.send({
